ea8ab2c1e23d1b892653ba1749e7aec14780dff8
Harsh Shandilya <me@msfjarvis.dev>
2021-05-15 20:02:43 +0530

    Arrays
    Signed-off-by: Harsh Shandilya <me@msfjarvis.dev>

--- src/main.zig
+++ src/main.zig
@@ -9,4 +9,7 @@ const Vec3 = struct {
 pub fn main() void {
     const inferred_constant: Vec3 = undefined;
     std.debug.print("Hello, {d}!\n", .{inferred_constant});
+
+    const implicitly_sized_array = [_]u8{0, 1, 2, 3};
+    std.debug.print("This is an array: {}\n", .{implicitly_sized_array});
 }
