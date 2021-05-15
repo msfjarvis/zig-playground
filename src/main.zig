5249589f1841b23e09fe365fea086a31168ad353
Harsh Shandilya <me@msfjarvis.dev>
2021-05-15 20:07:04 +0530

    Testing
    Signed-off-by: Harsh Shandilya <me@msfjarvis.dev>

--- src/main.zig
+++ src/main.zig
@@ -13,3 +13,9 @@ pub fn main() void {
     const implicitly_sized_array = [_]u8{0, 1, 2, 3};
     std.debug.print("This is an array: {}\n", .{implicitly_sized_array});
 }
+
+
+test "implicitly sized array length" {
+    const arr = [_]u8{10, 20, 30, 40};
+    std.testing.expect(arr.len == 4);
+}
