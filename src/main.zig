4903761faefed777b3c0468e822018ec298837be
Harsh Shandilya <me@msfjarvis.dev>
2021-05-15 20:10:54 +0530

    fixup! Arrays

--- src/main.zig
+++ src/main.zig
@@ -10,7 +10,7 @@ pub fn main() void {
     const inferred_constant: Vec3 = undefined;
     std.debug.print("Hello, {d}!\n", .{inferred_constant});
 
-    const implicitly_sized_array = [_]u8{0, 1, 2, 3};
+    const implicitly_sized_array = [_]u8{ 0, 1, 2, 3 };
     std.debug.print("This is an array: {}\n", .{implicitly_sized_array});
 }
 
